1fa1ae8dd006b7f92a91e3147fdd676703e6c888
Nimor111 <georgi.bojinov@hotmail.com>
2021-03-19 20:12:59 +0200

    Add dmenu

--- home.nix
+++ home.nix
@@ -40,6 +40,7 @@ in
     pkgs.ripgrep
     pkgs.termonad-with-packages
     pkgs.xmobar
+    pkgs.dmenu
 
     (let neuronRev = "44855fb8674e74a6b9a6688a8dff0298e9c78124";
         neuronSrc = builtins.fetchTarball "https://github.com/srid/neuron/archive/${neuronRev}.tar.gz";
